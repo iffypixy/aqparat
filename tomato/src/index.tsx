cc855564b2130a478df07f2d34fc55964863cdf6
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-22 00:31:28 +0600

    chore: render app

--- tomato/src/index.tsx
+++ tomato/src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import {createRoot} from "react-dom/client";
 
+import {App} from "./app";
+
 const element = document.getElementById("root");
 const root = createRoot(element as Element);
 
-root.render(<></>);
+root.render(<App />);
