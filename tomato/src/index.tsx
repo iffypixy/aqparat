dd4b1da6d37b2fa0ca3c8620b4bc54b20fa92653
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-21 20:54:58 +0600

    chore: add index point

---
+++ tomato/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import {createRoot} from "react-dom/client";
+
+const element = document.getElementById("root");
+const root = createRoot(element as Element);
+
+root.render(<></>);
