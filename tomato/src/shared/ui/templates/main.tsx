72f7b7cbd9d233340a435c5e4f62a300fd9f5aaf
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-23 00:26:35 +0600

    feat: add main template

---
+++ tomato/src/shared/ui/templates/main.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+
+export interface MainTemplateProps {
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export const MainTemplate: React.FC<MainTemplateProps> = ({
+  footer,
+  header,
+  children,
+}) => (
+  <Wrapper>
+    {header && <Header>{header}</Header>}
+    <Main>{children}</Main>
+    {footer && <Footer>{footer}</Footer>}
+  </Wrapper>
+);
+
+const Wrapper = styled("div")`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled("header")`
+  width: 100%;
+`;
+
+const Main = styled("main")`
+  width: 100%;
+`;
+
+const Footer = styled("footer")`
+  width: 100%;
+`;
