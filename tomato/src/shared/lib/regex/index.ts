7d60c7f200f3859640b454a3ef9b7d1bd441f644
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-27 17:37:31 +0600

    feat: add lib regex

---
+++ tomato/src/shared/lib/regex/index.ts
@@ -0,0 +1,4 @@
+const email =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+export const regex = {email};
