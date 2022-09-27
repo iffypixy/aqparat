c9742e95c7d303cdeb7d4ae052dbe14783483ce4
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-27 17:30:33 +0600

    chore: add ui input

---
+++ tomato/src/shared/ui/atoms/input.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Input = styled("input")`
+  border: none;
+  background: none;
+  outline: none;
+  border-bottom: 2px solid #2d2d2d;
+  padding: 1rem 1.5rem;
+
+  &::placeholder {
+    color: #747474;
+  }
+`;
