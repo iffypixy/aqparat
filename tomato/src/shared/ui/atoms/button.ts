b6783618f9aac3898e9b10002f1fbaa60514a2fc
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-27 17:30:06 +0600

    feat: add ui button

---
+++ tomato/src/shared/ui/atoms/button.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Button = styled("button")`
+  color: #2d2d2d;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 700;
+  background-color: #fdb25a;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  padding: 1.5rem 2.5rem;
+`;
