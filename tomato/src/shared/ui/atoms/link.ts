de398a57cd52c49f73250d996e4818d2c5a74a41
Ansat Jartybayev <ansatjr@gmail.com>
2022-09-27 17:29:49 +0600

    feat: add ui link

---
+++ tomato/src/shared/ui/atoms/link.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import {Link as RouterLink} from "react-router-dom";
+
+export const Link = styled(RouterLink)`
+  color: #2d2d2d;
+  text-decoration: none;
+`;
